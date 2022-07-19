58d618a72d92d658d31c5be2cf3dfe6178256c42
Scott Robb <scottrobb10@gmail.com>
2022-07-19 14:06:33 +0100

    .

--- public/main.js
+++ public/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         if (this.scrollY > 6500 && screen.width > 947){
             $(".scroll-down-btn").removeClass("show");
         }
-        else if (this.scrollY > 6700 && screen.width > 500){
+        else if (this.scrollY > 7000 && screen.width > 0){
             $(".scroll-down-btn").removeClass("show");
         }
         else if (this.scrollY > 500){
